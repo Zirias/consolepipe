9607ed6cf4c3b97a41841e7dfc8d47fd755a10a2
Felix Palmen <felix@palmen-it.de>
2016-03-21 17:18:14 +0100

    initial commit

---
+++ consolepipe.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <curses.h>
+
+#define ERR_SECONDS 60
+
+char buf[1024];
+FILE *pipef = 0;
+
+static void done(void)
+{
+    endwin();
+    if (pipef) fclose(pipef);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+	fprintf(stderr, "Usage: %s /path/to/fifo\n", argv[0]);
+	return EXIT_FAILURE;
+    }
+
+    initscr();
+    atexit(done);
+    noecho();
+    curs_set(0);
+    scrollok(stdscr, 1);
+
+    int failcount = 0;
+    while (1)
+    {
+        pipef = fopen(argv[1], "r");
+
+        if (!pipef)
+        {
+            if (failcount) --failcount;
+            else
+            {
+                failcount = ERR_SECONDS;
+                attron(A_BOLD);
+                printw("ERROR: cannot open pipe `%s' for reading: %s\n",
+                        argv[1], strerror(errno));
+                attroff(A_BOLD);
+		refresh();
+            }
+            sleep(1);
+            continue;
+        }
+
+	failcount = 0;
+        while (fgets(buf, 1024, pipef))
+        {
+            addstr(buf);
+            refresh();
+        }
+        fclose(pipef);
+        pipef = 0;
+    }
+
+}
+
